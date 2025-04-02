305b9d527b21bb4c8293b8be9a426c8cb747cf3a
Valkony Balazs (XC/ENG4-Bp) <Balazs.Valkony@hu.bosch.com>
2025-04-02 10:23:54 +0200

    feat: Add Vercel Speed Insights to application

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
 import { Analytics } from '@vercel/analytics/vue'
+import { SpeedInsights } from '@vercel/speed-insights/vue'
 </script>
 
 <template>
   <Analytics />
+  <SpeedInsights />
   <header>
     <div class="header-content">
       <div class="logo-container">
